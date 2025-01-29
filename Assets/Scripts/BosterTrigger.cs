e81eb68f59dbc44bf3391bcbe1e95d9d3a724e4f
LuciaPosada <lpgvigo@movistar.es>
2025-01-29 10:55:00 +0100

    Finalizado sript

--- Assets/Scripts/BosterTrigger.cs
+++ Assets/Scripts/BosterTrigger.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BosterTrigger : MonoBehaviour {
-    
-    public float aumentoVelocidad;
-    public float duracion;
-    public PlayerController player;
-    private void OnTriggerEnter(Collider colision) {
+    public float aumentoVelocidad; 
+    public float duracion; 
 
-        if(colision.CompareTag("Player")){
-            player.speed += aumentoVelocidad;
+    private void OnTriggerEnter(Collider colision){
+
+        if (colision.CompareTag("Player")){
+            PlayerController player = colision.GetComponent<PlayerController>();
+            StartCoroutine(Boost(player)); // Corrutina
         }
+    }
+
+    private IEnumerator Boost(PlayerController player){
+        player.speed += aumentoVelocidad;
+
+        // Espera el tiempo del boost
+        yield return new WaitForSeconds(duracion);
+
+        player.speed -= aumentoVelocidad;
     }
 }
